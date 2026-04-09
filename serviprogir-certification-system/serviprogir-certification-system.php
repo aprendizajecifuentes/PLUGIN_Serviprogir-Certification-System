5d0039de7bf38c491f92bba81f160d9651236a1c
Portafolio-Juan-Cifuentes. <aprendizaje.cifuentes@gmail.com>
2026-04-09 08:32:13 +0000

    Short code Alumno certificacion visalizacion

--- serviprogir-certification-system/serviprogir-certification-system.php
+++ serviprogir-certification-system/serviprogir-certification-system.php
@@ -2,15 +2,16 @@
 /**
  * Plugin Name: Serviprogir Certification System
  * Description: Panel docente + validación + certificados LearnDash con trazabilidad de fechas.
- * Version: 1.7.5
+ * Version: 1.7.2
  * Author: Juan Cifuentes
  */
 
 if (!defined('ABSPATH')) exit; // Seguridad básica
 
-// 1. Cargar dependencias (Los otros archivos)
+// 1. Cargar dependencias (Nuestros módulos)
 require_once plugin_dir_path(__FILE__) . 'includes/scs-excel-export.php';
 require_once plugin_dir_path(__FILE__) . 'includes/scs-panel-docente.php';
+require_once plugin_dir_path(__FILE__) . 'includes/scs-shortcode-alumno.php'; // <-- NUEVA LÍNEA
 
 // 2. Cargar CSS y JS globales del plugin
 add_action('wp_enqueue_scripts', 'scs_enqueue_scripts');
